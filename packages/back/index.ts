178ab913778fed34706233b3582c6f9296d62db7
Sébastien LEONCE <sebastienleonce91@gmail.com>
2022-01-30 13:33:47 +0100

    fix(back) ts-node/register

--- packages/back/index.ts
+++ packages/back/index.ts
@@ -2,6 +2,9 @@ import { Server } from "socket.io";
 import * as dotenv from 'dotenv'; dotenv.config();
 import db from './plugins/db'; db;
 
+require('ts-node/register')
+require('tsconfig-paths/register')
+
 import { 
     ClientToServerEvents, 
     ServerToClientEvents, 
